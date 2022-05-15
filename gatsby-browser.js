940b1588070d52db6058aedec6a3cc03f562da59
Simon vom Eyser <simon.vom.eyser@gmail.com>
2022-05-15 18:24:55 +0200

    feat: add 100vh calculation

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,14 @@
 import './src/styles/global.css'
+
+// Calculate the actual size of the 100vh, including bottom bars etc. on mob devices
+// https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
+if (typeof window !== 'undefined') {
+
+    const appHeight = () => {
+        const doc = document.documentElement
+        doc.style.setProperty('--100vh', `${window.innerHeight}px`)
+    }
+    window.addEventListener('resize', appHeight, {passive: true})
+    appHeight()
+
+}
