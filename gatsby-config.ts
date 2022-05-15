cba59d2ff68dd601e5d47260c2938bd20b562716
Simon vom Eyser <simon.vom.eyser@gmail.com>
2022-05-15 16:16:54 +0200

    feat: install tailwind

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,30 +1,32 @@
-import type { GatsbyConfig } from "gatsby";
+import type {GatsbyConfig} from "gatsby";
 
 const config: GatsbyConfig = {
-  siteMetadata: {
-    title: `Shearer Creative Studio`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-transformer-remark", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+    siteMetadata: {
+        title: `Shearer Creative Studio`,
+        siteUrl: `https://shearer.studio`
     },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
+    plugins: ["gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", 'gatsby-plugin-postcss', {
+        resolve: 'gatsby-plugin-manifest',
+        options: {
+            "icon": "src/images/icon.png"
+        }
+    }, "gatsby-transformer-remark", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+        resolve: 'gatsby-source-filesystem',
+        options: {
+            "name": "images",
+            "path": "./src/images/"
+        },
+        // @ts-ignore
+        __key: "images"
+    }, {
+        resolve: 'gatsby-source-filesystem',
+        options: {
+            "name": "pages",
+            "path": "./src/pages/"
+        },
+        // @ts-ignore
+        __key: "pages"
+    }]
 };
 
 export default config;
