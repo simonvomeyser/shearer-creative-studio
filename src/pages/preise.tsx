1c48a83a5f1506bcd2215d95ae51bf84b45ecb69
Simon vom Eyser <simon.vom.eyser@gmail.com>
2022-05-18 12:32:46 +0200

    feat: add placeholder pages

---
+++ src/pages/preise.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {MetaTags} from "../components/MetaTags";
+
+const Preise = () => {
+
+    return (
+        <>
+            <MetaTags pageName="Preise"/>
+            <section className="h-screen text-8xl justify-center items-center flex ">
+                Preise
+            </section>
+        </>
+    )
+};
+export default Preise
+
---
+++ src/pages/termin.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {MetaTags} from "../components/MetaTags";
+
+const Termin = () => {
+
+    return (
+        <>
+            <MetaTags pageName="Termin"/>
+            <section className="h-screen text-8xl justify-center items-center flex ">
+                Termin
+            </section>
+        </>
+    )
+};
+export default Termin
+
