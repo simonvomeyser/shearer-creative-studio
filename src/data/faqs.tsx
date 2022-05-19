c596d57ea630e16f1a247fd02565ec31631cd6df
Simon vom Eyser <simon.vom.eyser@gmail.com>
2022-05-19 08:26:26 +0200

    feat: add a few faq contents

--- src/data/faqs.tsx
+++ src/data/faqs.tsx
@@ -13,8 +13,23 @@ export const faqs: FaqType[] = [
         answer: ' Condimentum massa eros eu scelerisque dictum platea vulputate blandit placerat mattis, conubia suspendisse sociosqu felis mi posuere convallis libero bibendum tortor, lacus interdum laoreet pellentesque erat faucibus potenti mus imperdiet. Iaculis maximus nunc tempus erat augue suscipit donec <br><br> massa dis quam scelerisque, inceptos id fermentum hac felis eget ex conubia duis.'
     },
     {
-        question: 'Warum möchtest du keine Barzahlung?',
-        answer: 'Sit adipiscing etiam et fermentum ultrices laoreet <br> libero tellus sem iaculis dapibus venenatis suscipit lorem porttitor tortor urna, aliquam integer praesent taciti erat nam odio mollis sapien montes nullam lacinia interdum magnis at. Condimentum massa eros eu scelerisque dictum platea vulputate blandit placerat mattis, conubia suspendisse sociosqu felis mi posuere convallis libero bibendum tortor, lacus interdum laoreet pellentesque erat faucibus potenti mus imperdiet. Iaculis maximus nunc tempus erat augue suscipit donec <br><br> massa dis quam scelerisque, inceptos id fermentum hac felis eget ex conubia duis.'
+        question: 'Warum muss ich meinen Termin anzahlen? ',
+        answer: `
+       <b>Die Anzahlung dient uns zur sicherheit, das du es auch ernst mit uns meinst. </b>
+    Wir respektieren eure Zeit somit wollen wir das ihr das auch mit unserer tut. <br><br>
+Du kannst deinen Termin 48h/2 Tage vorher kostenlos per Telefon/Mail/WhatsApp stornieren oder verschieben. Für Absagen, die nicht innerhalb unserer Stornofrist liegen, müssen wir die Anzahlung einbehalten, da wir diese Termine so kurzfristig nicht wieder verbucht bekommen.
+Um euch durch kurzfristige Terminabsagen anfallende Kosten zu ersparen, seit euch sicher, das ihr euren Termin auch wahnehmen könnt.
+ 
+        `
+    },
+    {
+        question: 'Mist, ich komme zu spät. was nun? ',
+        answer: `
+     Das ist natürlich nicht der hit aber auch kein Weltuntergang. Shit happens. Ruft uns kurz an oder schreibt uns, dann wissen wir zumindest schon mal Bescheid, dass es später wird. Wir versuchen natürlich dann in deiner gebuchten Zeit deine Wünsche umzusetzten. <br> <br>Sollte die Verspätung aber größer ausfallen, behalten wir uns vor die zusätzlich benötigte Zeit in Rechung zu stellen. 
+    <br><br> 
+    <span class="text-primary">Unter Zeitdruck ist noch nie eine gute Frisur entstanden ;)</span> 
+
+        `
     },
     {
         question: 'Imperdiet aliquet netus cubilia elementum lobortis torquent',
