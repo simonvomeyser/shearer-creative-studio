a957e9361198390f4aa9450aa13a867b2828a442
Simon vom Eyser <simon.vom.eyser@gmail.com>
2022-05-23 12:58:35 +0200

    feat: remove face in header animation

--- src/components/Header/components/HeaderDesktop.tsx
+++ src/components/Header/components/HeaderDesktop.tsx
@@ -1,6 +1,5 @@
 import clsx from "clsx";
 import {Link} from "gatsby";
-import {motion} from "framer-motion";
 import React from 'react';
 import {Logo} from "../../Logo";
 import {MenuLink} from "./MenuLink";
@@ -13,11 +12,7 @@ export const HeaderDesktop: React.FC<HeaderDesktopProps> = ({className}) => {
 
     return (
         <div className={clsx('', className)}>
-            <motion.ul
-                initial={{opacity: 0, translateY: '-5%'}}
-                animate={{opacity: 1, translateY: 0}}
-                transition={{delay: 0.5, duration: 0.5}}
-                className="justify-center items-center h-full text-xl font-regular text-gray-50 flex">
+            <ul className="justify-center items-center h-full text-xl font-regular text-gray-50 flex">
                 <MenuLink linkClassName="p-8" href="/salon">Salon</MenuLink>
                 <MenuLink linkClassName="p-8" href="/preise">Preise</MenuLink>
                 <li className="w-[275px] mx-8">
@@ -27,7 +22,7 @@ export const HeaderDesktop: React.FC<HeaderDesktopProps> = ({className}) => {
                 </li>
                 <MenuLink linkClassName="p-8" href="/diary">Diary</MenuLink>
                 <MenuLink linkClassName="p-8" href="/termin">Termin</MenuLink>
-            </motion.ul>
+            </ul>
         </div>
     );
 };
