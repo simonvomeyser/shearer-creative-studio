f068e09d7b32fc2f3f0d39445c09b65c070967c9
Simon vom Eyser <simon.vom.eyser@gmail.com>
2022-05-25 14:10:41 +0200

    feat: update quotes

--- src/data/quotes.ts
+++ src/data/quotes.ts
@@ -7,23 +7,21 @@ export type QuoteDataType = {
 export const quotes: QuoteDataType[] = [
     {
         body: 'I consider hair my weapon of choice when it comes to self-expression.',
-        author: 'Sarah Scherer',
-        preventCta: true,
     },
     {
         body: 'Die Qualität deiner Haare ist uns immer wichtiger als der neueste Trend',
-        author: 'Sarah Scherer',
     },
     {
         body: 'Invest in your hair - it\'s the crown you never take off.',
-        author: 'Sarah Scherer',
     },
     {
         body: 'Also komm! Lass uns deine Individualität feiern.',
-        author: 'Sarah Scherer',
     },
     {
         body: 'Individualität feiern, egal wo du dich auf dem Genderspektrum siehst!',
+    },
+    {
+        body: 'Leben heißt Veränderung sagte der Stein zur Blume und flog davon',
     },
 ];
 
