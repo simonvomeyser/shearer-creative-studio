2fb6300898475d78af8fad7f2f4ba4d533f9c3e2
Simon vom Eyser <simon.vom.eyser@gmail.com>
2022-06-06 13:38:47 +0200

    feat: adjust content

--- src/pages/impressum.tsx
+++ src/pages/impressum.tsx
@@ -17,9 +17,13 @@ const Impressum = () => {
           <p>Telefon: {globalData.phoneDisplay}<br />
               E-Mail: {globalData.email}</p>
 
+
+          <div className="hidden">
+          {/*    @todo */}
           <h2>Umsatzsteuer-ID</h2>
           <p>Umsatzsteuer-Identifikationsnummer gem&auml;&szlig; &sect; 27 a Umsatzsteuergesetz:<br />
               DE999999999</p>
+          </div>
 
           <h2>Aufsichtsbeh&ouml;rde</h2>
           <p>Handwerkskammer D&uuml;sseldorf<br />
@@ -27,12 +31,15 @@ const Impressum = () => {
               40221 D&uuml;sseldorf</p>
           <p><a href="https://www.hwk-duesseldorf.de/" target="_blank" rel="noopener noreferrer">https://www.hwk-duesseldorf.de/</a></p>
 
+          <div className="hidden">
+          {/*    @todo */}
           <h2>Angaben zur Berufs&shy;haftpflicht&shy;versicherung</h2>
           <p><strong>Name und Sitz des Versicherers:</strong><br />
               Beispiel Versicherung AG<br />
               Musterweg 10<br />
               90210 Musterstadt</p>
           <p><strong>Geltungsraum der Versicherung:</strong><br />Deutschland</p>
+          </div>
 
           <h2>Redaktionell verantwortlich</h2>
           <p>Sarah Scherer<br />
@@ -45,7 +52,11 @@ const Impressum = () => {
           <h2>Verbraucher&shy;streit&shy;beilegung/Universal&shy;schlichtungs&shy;stelle</h2>
           <p>Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.</p>
 
-          <p>Quelle: <a href="https://www.e-recht24.de/impressum-generator.html">https://www.e-recht24.de/impressum-generator.html</a></p>
+          <h2>Design & Umsetzung</h2>
+          <p><b>Branding:</b> Kathi Link, Felix Rostig, Damian Brzostewicz </p>
+          <p><b>Screendesign:</b> Felix Rostig, Damian Brzostewicz </p>
+          <p><b>Technische Umsetzung:</b> <a href="https://simonvomeyser.de"  target="_blank" rel="noopener noreferrer">Simon vom Eyser</a></p>
+
       </PlainTextPage>
   );
 };
