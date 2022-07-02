2584865b10a856115e187cc712503c70674eeb23
Simon vom Eyser <simon.vom.eyser@gmail.com>
2022-07-02 11:01:07 +0200

    feat: add prices 1.0

--- src/data/prices.ts
+++ src/data/prices.ts
@@ -17,28 +17,28 @@ export const priceCategories: PriceCategoryType[] = [
         "anchor": "cut",
         "detailPageSorting": 0,
         "description": ` Dein Haarschnitt, ist genau so Individeuell wie du. 
-Deswegen berechnen wir den Haarschnitt nach benötigtem Zeitaufwand und Komplexität sowie gewünschten Serviceleistungen.    
+Deswegen berechnen wir  nach benötigtem Zeitaufwand und Komplexität sowie gewünschten Serviceleistungen.    
         `,
         "prices": [
             {
-                "title": "Just the litte things in life ",
-                "price": "26 €",
-                "description" : "Bis 25min (Beispiel: Pony nach scheiden, Konturenschnitt, Maschinenschnitt) "
+                "title": "Just the litte things in life",
+                "price": "24 €",
+                "description" : "Einfacher Haarschnitt bis 25 Min. Beispiel: Pony nachscheiden, Konturenschnitt, Maschinenschnitt"
             },
             {
-                "title": "Keep it simple please",
-                "price": "46 €",
-                "description" : "Bis 45min (Beispiel: Waschen, Schneiden, Pflegen, Stylen von einfachen Haarschnitten)"
+                "title": "Keep it simple",
+                "price": "43 €",
+                "description" : "Alles bis 45min. Beispiel: Waschen, Schneiden, Pflegen, Stylen von einfachen Haarschnitten wie Kurzhaarschnitte / Spitzenschnitt o. Ä."
             },
             {
-                "title": "Der Klassiker",
-                "price": "61€",
-                "description": "Beispiel: Waschen, Schneiden, Pflegen, Stylen von Komplexen Haarschnitten"
+                "title": "Golden Hour",
+                "price": "57 €",
+                "description": "Der Klassiker, alles bis 60min. Beispiel: Waschen, Schneiden, Pflegen, Stylen von komplexen Haarschnitten wie Bobs, Stufenhaarschnitten, Mullets o. Ä."
             },
             {
                 "title": "I want it all. I want it now",
-                "price": "76€",
-                "description": 'Bis 75min( Beispiel:Waschen, Schneiden, Pflegen, Stylen von Neuhaarschnitten bzw.Typveränderungen '
+                "price": "73 €",
+                "description": "Größere Dienstleistungen bis 75min. Beispiel:Waschen, Schneiden, Pflegen, Stylen von Neuhaarschnitten bzw. Typveränderungen"
 
             }
         ]
@@ -47,44 +47,64 @@ Deswegen berechnen wir den Haarschnitt nach benötigtem Zeitaufwand und Komplexi
         "title": "Color",
         "anchor": "color",
         "detailPageSorting": 0,
-        "description": `Alle Farbdienstleistungen verstehen sich immer inkl. passenem Haarpflege Treatment <br><br>
-Bei Farbterminen, die keinen Haarschnitt mit einbeziehen, behalten wir uns vor XX€ als Föhnpauschale zu berechnen. 
-`,
+        "description": `Alle unserer Farbdienstleistungen verstehen sich inkl. passener Pflegeanwendung. <br/>
+Bei Farbterminen, die keinen Haarschnitt mit einbeziehen, behalten wir uns vor 14€ als Föhnpauschale zu berechnen. `,
         "prices": [
             {
                 "title": "Ansatzfärbung bis 2cm",
-                "price": "XXX"
+                "price": "59 €"
             },
             {
                 "title": "Ansatzfärbung ab 2cm",
-                "price": "XXX"
+                "price": "64 €"
             },
             {
                 "title": "Längenausgleich",
-                "price": "XXX - XXX",
+                "price": "29 € - 63 €",
                 "description": "Auffrischung von verblassten Längen und Spitzen"
             },
             {
                 "title": "Ansatz Blondierung bis 8 Wochen",
-                "price": "XXX",
+                "price": "98 €",
                 "description": "inkl. Gloss"
             },
             {
                 "title": "Neublondierung",
-                "price": "XXX",
+                "price": "ab 129 €",
                 "description": "inkl. Gloss"
             },
             {
                 "title": "Gloss",
-                "price": "XXX",
+                "price": "36 €",
             },
-            { "title" : "Faceframe", price: "XXX" },
-            { "title" : "Fashion Color", price:  "ab 79€/h" },
+            {
+                "title" : "Faceframe",
+                "price": "66 €"
+            },
+            {
+                "title" : "Fashion Color",
+                "price":  "ab 79 € / Std"
+            },
 
-            { "title" : "Highlights 1/3", price: "XXX" },
-            { "title" : "Highlights 2/3", price: "XXX" },
-            { "title" : "Highlights 3/3", price: "XXX" , description: "Highlights sind inkl. Gloss/Rootshadow" },
-            { "title" : "Farbkorrekturen", price: "89 € / Std" },
+            {
+                "title" : "Highlights 1/3",
+                "price": "69 €"
+            },
+            {
+                "title" : "Highlights 2/3",
+                "price": "89 €"
+            },
+            {
+                "title" : "Highlights 3/3",
+                "price": "129 € " , description: "Highlights sind inkl. Gloss/Rootshadow" },
+            {
+                "title" : "Farbkorrekturen",
+                "price": "89 € / Std"
+            },
+            {
+                "title" : "Grauhaarkaschierung",
+                "price": "22 €"
+            },
 
         ]
     },
@@ -92,16 +112,18 @@ Bei Farbterminen, die keinen Haarschnitt mit einbeziehen, behalten wir uns vor X
         "title": "Balayage",
         "anchor": "balayage",
         "detailPageSorting": 0,
-        "description": "Alle Balayage Pakete verstehen sich inkl. Olaplex, Glossing, passendem Pflegetreatment. und exkl. Haarschnitt ",
+        "description": "Alle Balayage Pakete sind inkl. Olaplex, Glossing, passender Pflegeanwendung und exkl. Haarschnitt.",
 
         "prices": [
             {
-                "title": "Natural Balayage (ca. 150min)",
-                "price": "ab 140€"
+                "title": "Natural Balayage",
+                "price": "ab 140€",
+                "description": "Dauer ca. 150min"
             },
             {
-                "title": "Fashion Balayage (ca. 180min)",
-                "price": "ab 190€"
+                "title": "Fashion Balayage",
+                "price": "ab 190€",
+                "description": "Dauer ca. 180min"
             },
         ]
     },
@@ -110,15 +132,15 @@ Bei Farbterminen, die keinen Haarschnitt mit einbeziehen, behalten wir uns vor X
         "anchor": "beratung",
         "detailPageSorting": 1,
         "description": `
-            Du bist dir noch unsicher, was dir steht oder du wünschst ein komplettes Make-Over? <br><br> 
-            Du brächtest einen professionellen Rat bezüglich deiner Heimpflege oder brauchst eine ehrliche Einschätzung zu einer Farbkorrektur? 
+            Du bist dir noch unsicher, was dir steht oder wünscht dir ein komplettes Make-Over? <br><br> 
+            Du brauchst einen professionellen Rat bezüglich deiner Haarpflege oder eine ehrliche Einschätzung zu einer Farbkorrektur? 
          `,
         "prices": [
             {
                 "title": "Typgerechte Beratung",
                 "description": `
-                    Wir setzen uns zusammen und du bekommst eine persönliche Beratung! 
-                    Die Beratung wird im Falle einer Folgebuchung mit der nächsten Dienstleistung verrechnet.<br>
+                    Komm vorbei und lass dich persönlich beraten! <br/>
+                    Im Falle einer Folgebuchung wird die Beratung mit der nächsten Dienstleistung verrechnet. <br>
                 `,
                 "price": "19€ / 20 Min",
             },
@@ -129,70 +151,48 @@ Bei Farbterminen, die keinen Haarschnitt mit einbeziehen, behalten wir uns vor X
         "anchor": "care",
         "detailPageSorting": 0,
         "prices": [
-            {"title": "Grün", "price": "Umsonst"},
-            {"title" : "Olaplex Treatment", price: "XXX"},
-            {"title" : "K18", price: "XXX"},
-            {"title" : "Detox", price: "XXX"},
-            {"title" : "Styling", price: "XXX"},
-            {"title" : "Blow Dry", price: "XXX"},
-            {"title" : "Fashion Styling", price: "XXX"},
-            {"title" : "Hochstecken", price: "XXX"},
-            {"title" : "Beauty", price: "XXX"},
-            {"title" : "Brow", price: "15 €"},
-            {"title" : "Lashes", price: "XXX"},
-            {"title" : "Make-up", price: "XXX"	},
+            {"title": "Olaplex Rebuild Treatment", "price": "ab 28/39/55 €"},
+            {"title" : "Olaplex Shot Color Addon", price: "14 €"},
+            {"title" : "K18", price: "28 €"},
+            {"title" : "Detox", price: "16 €"},
         ]
     },
     {
-        "title": "Styling",
-        "anchor": "styling",
+        "title": "Sonstiges",
+        "anchor": "sonstiges",
         "detailPageSorting": 0,
         "prices": [
             {
-                "title": "xxx",
-                "price": "xxx"
+                "title": "Blow Dry",
+                "price": "38 €",
+                "description": "Dauer ca. 40 Min"
             },
             {
-                "title": "xxx",
-                "price": "xxx"
+                "title": "Fashion Styling",
+                "price": "29 €",
+                "description": "Dauer ca. 30 Min"
             },
             {
-                "title": "xxx",
-                "price": "xxx"
-            },
-        ]
-    },
-    {
-        "title": "Beauty",
-        "anchor": "beauty",
-        "detailPageSorting": 0,
-        "prices": [
-            {
-                "title": "xxx",
-                "price": "xxx"
+                "title": "Hochstecken",
+                "price": "ab 47 €"
             },
             {
-                "title": "xxx",
-                "price": "xxx"
+                "title": "Brow",
+                "price": "15 €",
             },
             {
-                "title": "xxx",
-                "price": "xxx"
+                "title": "Lashes",
+                "price": "15 €",
             },
-        ]
-    },
-    {
-        "title": "Extensions",
-        "anchor": "extensions",
-        "detailPageSorting": 0,
-        "prices": [
             {
-                title: "Preise auf Anfrage",
-                price: "-",
+                "title": "Make-up",
+                "price": "ab 38 € / 56 €"
+            },
+            {
+                title: "Extensions",
+                price: "Preis auf Anfrage",
                 description: `Kontaktiere uns einfach , und wir besprechen deinen Preis!`
             },
-            { "title" : "Highlights 3/3", price: "XXX" , description: "Highlights sind inkl. Gloss/Rootshadow" },
-
         ]
     }
 
