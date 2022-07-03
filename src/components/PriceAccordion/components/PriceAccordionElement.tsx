46821f61dd45967922d7074d68b8603225e85b7e
Simon vom Eyser <simon.vom.eyser@gmail.com>
2022-07-03 12:46:07 +0200

    fix: remove duplicate text from dom

--- src/components/PriceAccordion/components/PriceAccordionElement.tsx
+++ src/components/PriceAccordion/components/PriceAccordionElement.tsx
@@ -114,10 +114,12 @@ export const PriceAccordionElement: React.FC<PriceAccordionElementProps> = (
                         animate={isCurrent(id) ? 'open' : 'closed'}>
                 <div className="py-6 space-y-2 ">
 
+                    {isCurrent(id) ?
                     <motion.div className="mb-4 max-w-[50ch]" variants={readMoreMotionVariant}>
                         Unsere Preise werden fair und transparent berechnet - und vielleicht anders, als du es
                         kennst! <Link to="/preise" className="underline transition duration-500 hover:text-primary ">Erfahre
                         mehr</Link></motion.div>
+                    : null}
 
                     {category.prices.map((price, priceId) => (
                         <motion.div key={`${id}.${priceId}`}
