d2699144bbe1d43dc1b17a02482f604fa088ea7d
Simon vom Eyser <simon.vom.eyser@gmail.com>
2022-07-03 17:46:52 +0200

    feat: add new faq content

--- src/data/faqs.tsx
+++ src/data/faqs.tsx
@@ -7,85 +7,86 @@ export const faqs: FaqType[] = [
     {
         question: 'Warum gender neutral und nicht "unisex"?',
         answer: `
-             Haare sind nicht an ein Geschlechtsmerkmal gebunden. Unisex impliziert für uns das es nur “Mann” und “Frau” gibt. Aber Haaren ist es egal wie du dich identifiziert. 
-             <br><br> 
-             <span class="italic">
-             Wir wollen deine Individualität feiern, egal wo du dich auf dem Genderspektrum siehst!
-             </span> 
+Haare sind nicht an ein Geschlechtsmerkmal gebunden. Unisex impliziert für uns das es nur “Mann” und “Frau“ gibt. Aber Haaren ist es egal wie du dich identifizierst.
+<br><br> 
+<b>Wir wollen deine Individualität feiern, egal wo du dich auf dem Genderspektrum siehst!</b>
         `
     },
     {
         question: 'Wie setzten sich die Preise zusammen?',
         answer: `
-            Wir berechnen unsere Dienstleistungen ausschließlich nach Zeit, Skill und Material Aufwand. Wir besprechen mit dir im Vorfeld, wie sich die Preise individuell zusammensetzen. 
-            Eine genauere Beschreibung findest du auch  <a href="/preise"> auf unserer Preise-Seite</a>.
+Wir berechnen unsere Dienstleistungen ausschließlich nach Zeit, Komplexität und Materialaufwand. Wir besprechen mit dir im Vorfeld, wie sich die Preise individuell zusammensetzen. 
+<br>Eine genauere Beschreibung findest du auch  <a href="/preise"> auf unserer Preise-Seite</a>.
         `
     },
     {
         question: 'Warum muss ich meinen Termin anzahlen? ',
         answer: `
-       <b>Die Anzahlung dient uns zur sicherheit, das du es auch ernst mit uns meinst. </b>
-    Wir respektieren eure Zeit somit wollen wir das ihr das auch mit unserer tut. <br><br>
-Du kannst deinen Termin 48h/2 Tage vorher kostenlos per Telefon/Mail/WhatsApp stornieren oder verschieben. Für Absagen, die nicht innerhalb unserer Stornofrist liegen, müssen wir die Anzahlung einbehalten, da wir diese Termine so kurzfristig nicht wieder verbucht bekommen.
-Um euch durch kurzfristige Terminabsagen anfallende Kosten zu ersparen, seit euch sicher, das ihr euren Termin auch wahnehmen könnt.
- 
+ Wir wollen sicher sein, dass du es auch ernst mit uns meinst. Wir respektieren deine Zeit und wünschen uns, dass ihr mit unserer ebenso umgeht.
+<br><br> 
+Du kannst deinen Termin 48 Stunden/2 Tage vorher kostenlos per Telefon/Mail/WhatsApp stornieren oder verschieben. Bei Absagen außerhalb der Stornofrist ist eine Rückzahlung der Anzahlung nicht mehr möglich, da wir deinen Termin so kurzfristig nicht mehr verbucht bekommen.
+<br><br> 
+Um euch die durch kurzfristige Terminabsagen anfallenden Kosten zu ersparen, seid euch sicher, dass ihr euren Termin auch wahrnehmen könnt. 
         `
     },
     {
         question: 'Mist, ich komme zu spät. was nun? ',
         answer: `
-         Das ist natürlich nicht der hit aber auch kein Weltuntergang. Shit happens. 
-         <br><br> 
-         Ruft uns kurz an oder schreibt uns, dann wissen wir zumindest schon mal Bescheid, dass es später wird. Wir versuchen natürlich dann in deiner gebuchten Zeit deine Wünsche umzusetzten. <br> <br>Sollte die Verspätung aber größer ausfallen, behalten wir uns vor die zusätzlich benötigte Zeit in Rechung zu stellen. 
-         <br><br> 
-         <span class="italic">Unter Zeitdruck ist noch nie eine gute Frisur entstanden!</span> 
-
+        Das ist natürlich nicht der Hit, aber auch kein Weltuntergang. Shit happens.
+<br><br> 
+Ruft uns einfach kurz an oder schreibt uns, dann wissen wir zumindest schon mal Bescheid, dass es später wird. Wir versuchen dann in deiner gebuchten Zeit deine Wünsche umzusetzen.
+Sollte die Verspätung aber größer ausfallen, behalten wir uns vor, die zusätzlich benötigte Zeit in Rechnung zu stellen.
+<br><br> 
+<b>Unter Zeitdruck ist noch nie eine gute Frisur entstanden!</b>
         `
     },
     {
         question: 'Welche Zahlungsarten bietet ihr an? ',
         answer: `
-            Wir bevorzugen bargeldloses Bezahlen, aber bieten auch die Möglichkeit, Bar zu bezahlen. <br><br>
-            <ul>
-<li>Barzahlung</li>
-<li>EC Karten</li>
-<li>Kreditkarte</li>
-<li>Paypal</li>
-</ul>
-        `
+Wir bevorzugen eine bargeldlose Bezahlung. Barzahlung ist aber natürlich möglich.
+Folgende Zahlungsarten bieten wir an :
+<br><br> 
+Barzahlung <br>
+EC-Karte <br> 
+Kreditkarte <br>
+Apple Pay / Google Pay <br> 
+PayPal <br>`
     },
     {
-        question: 'Was ist, wenn ich kein smalltalk führen will? ',
+        question: 'Was ist, wenn ich kein Smalltalk führen will? ',
         answer: `
-        Du möchtest zum Friseur gehen um einfach mal abzuschalten? Deine Nase in dein Buch zu stecken, Musik hören oder etwas ruhe zu genießen? 
-        <br><br> 
-        Garkein Problem. Nachdem wir dich Beraten haben und deine Wünsche klar sind, machen wir unsere Magie, ganz ohne zu quatschen! Sag uns einfach kurz bescheid, wenn dir das lieber ist. 
+        Du möchtest zum Friseur gehen, um einfach mal abzuschalten? Deine Nase in ein Buch stecken, Musik hören oder etwas Ruhe genießen?
+<br><br> 
+Gar kein Problem. Nachdem wir dich Beraten haben und deine Wünsche klar sind, wirken wir unsere Magie, ganz ohne zu quatschen. Sag uns einfach kurz Bescheid, wenn dir das lieber ist. 
+
         `
     },
     {
         question: 'Welche Produkte benutzt ihr? ',
         answer: `
-        Wir setzten auf höchste Qualität in jeden Belangen auch in unseren Produkten. 
-        <br><br> 
-        Wir arbeiten mit Davines, Schwarzkopf Professional, Pulp Riot, Olaplex uvm…
+        Wir setzen auf höchste Qualität, auch bei unseren Produkten.
+<br><br> 
+        Wir arbeiten mit Davines, Schwarzkopf Professional, Pulp Riot, EVO und Olaplex 
         `
     },
     {
         question: 'Gibt es etwas vor meinem Haarfärbetermin zu beachten? ',
         answer: `
-Plane genug Zeit ein. Blondierung arbeitet nicht schneller nur damit du schneller wieder ins Büro kommst. Haarschonende Farbtechniken brauchen ihre Zeit. 
-        <br><br> 
-Wir empfehlen dir auch 24-48h vor deinem Termin nicht mehr die Haare zu waschen, dies dient zum Schutz deiner Kopfhaut.
-        <br><br> 
-Bedenke auch, schöne Haarfarben kommen immer von zwei Seiten. Wir geben alles das du Happy aus dme Laden schwebst und du wäscht so selten wie dir möglich mit Produkten die auf deine Haare abgestimmt sind. Nur so hast du wirklich langfristig Freude daran. 
+Plane genug Zeit ein. Blondierung oder Farbe arbeiten nicht schneller, nur weil du es eilig hast wieder ins Büro zu kommen. Haarschonende Farbtechniken brauchen ihre Zeit.
+<br><br> 
+Wir empfehlen dir zudem 24-48h vor deinem Termin nicht mehr die Haare zu waschen. Dies dient zum Schutz deiner Kopfhaut.
+<br><br> 
+Bedenke auch, dass auch du einen Beitrag zu einer schönen und vor allem langanhaltenden Haarfarben leisten kannst. Wir geben alles, damit du glücklich aus dem Salon schweben kannst. Um langfristig Freude an deiner neuen Haarfarbe zu haben, empfehlen wir seltene Haarwäschen mit Produkten, die auf deine Haare abgestimmt sind
+
         `
     },
     {
         question: 'Mit welchen Verkehrsmitteln kann ich euch erreichen?',
         answer: `
-Es sind auf dem Nordwall gegen eine Parkgebühr reichlich Parkplätze vorhanden. 
-        <br><br> 
-Zudem hält die Straßenbahn 044 und der Bus 076 an der Haltestelle “Friedrichsplatz” 200m vom Salon entfernt. Vom Krefelder Hbf seit ihr mit Bus oder Bahn in 15min bei uns. Diese fahren Tagsüber ca. alle 15min 
+        Es sind auf dem Nordwall gegen eine Parkgebühr reichlich Parkplätze vorhanden.
+<br><br> 
+Zudem halten die Straßenbahn 044 und der Bus 076 an der Haltestelle “Friedrichsplatz” 200 m vom Salon entfernt. Vom Krefelder Hauptbahnhof seid ihr mit Bus oder Bahn in 15 min bei uns. Diese fahren Tagsüber ca. alle 15min. 
+
         `
     },
 
