f40874e722b4d80ab15c9949f9e627146feac4c5
Simon vom Eyser <simon.vom.eyser@gmail.com>
2023-11-22 19:38:50 +0100

    fix: error on building
    "document" is not available during Server-Side Rendering.
    Enable "DEV_SSR" to debug this during "gatsby develop".

--- src/lib/scrollLock.ts
+++ src/lib/scrollLock.ts
@@ -1,22 +1,23 @@
-const $body: HTMLBodyElement | null = document.querySelector('body');
+const isBrowser: boolean = typeof window !== "undefined";
+const $body: HTMLBodyElement | null = isBrowser ? document.querySelector("body") : null;
 let scrollPosition: number = 0;
 
 const scrollLock = {
   enable(): void {
     if ($body) {
       scrollPosition = window.scrollY;
-      $body.style.overflow = 'hidden';
-      $body.style.position = 'fixed';
+      $body.style.overflow = "hidden";
+      $body.style.position = "fixed";
       $body.style.top = `-${scrollPosition}px`;
-      $body.style.width = '100%';
+      $body.style.width = "100%";
     }
   },
   disable(): void {
     if ($body) {
-      $body.style.removeProperty('overflow');
-      $body.style.removeProperty('position');
-      $body.style.removeProperty('top');
-      $body.style.removeProperty('width');
+      $body.style.removeProperty("overflow");
+      $body.style.removeProperty("position");
+      $body.style.removeProperty("top");
+      $body.style.removeProperty("width");
       window.scrollTo(0, scrollPosition);
     }
   }
